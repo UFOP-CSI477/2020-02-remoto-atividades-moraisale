dc03baecdda283ce3b2a497ae01017b8567c672e
Alexandre Morais <alexandremorais26@gmail.com>
2021-07-13 10:01:16 -0300

    Adicionando exercícios

---
+++ Atividades/exercicios/exercicio-06/dados/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbfile = "./db/database.sqlite";
+$dbuser = "";
+$dbpassword = "";
+$dbhost = "";
+
+$strConnection = "sqlite:" . $dbfile;
+
+$connection = new PDO($strConnection, $dbuser, $dbpassword);
+
+$estados = $connection->query("SELECT * FROM estados");
